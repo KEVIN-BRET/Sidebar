54174f0f573dbab09f02d4d6167c90a9acb4838b
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-05-08 17:19:38 +0200

    Disparition de la sidebar au click a coté

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const sideBar = document.querySelector(".side-bar");
 // Filtre opaque devant le contenu :
 const darkFilter = document.querySelector(".dark-filter");
 
+// Ouverture/fermeture de la sidebar au click sur hamburger :
 btn.addEventListener("click", () => {
   sideBar.classList.toggle("visible");
   darkFilter.classList.toggle("dark-filter-on");
 });
 
 // Ranger la sidebar si on click sur le contenu principal
+
+// Disparition de la sidebar au click à coté :
+darkFilter.addEventListener("click", () => {
+  sideBar.classList.toggle("visible");
+  darkFilter.classList.toggle("dark-filter-on");
+});
 
 // BONUS
 // Créer en css l'hamburger menu qui se transforme en croix
