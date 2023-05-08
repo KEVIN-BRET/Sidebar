80e80696bbebed7dd4ebc7e9189a44c3c04e1d38
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-05-08 17:17:53 +0200

    Toggle display sidebar et content opacity OK

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
 
+// Side bar :
+const sideBar = document.querySelector(".side-bar");
+// Filtre opaque devant le contenu :
+const darkFilter = document.querySelector(".dark-filter");
 
+btn.addEventListener("click", () => {
+  sideBar.classList.toggle("visible");
+  darkFilter.classList.toggle("dark-filter-on");
+});
 
 // Ranger la sidebar si on click sur le contenu principal
 
