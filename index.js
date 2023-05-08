19cd64365456f9fdef9b9eec0bf1a6ba977f3b9d
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-05-08 19:00:11 +0200

    Correction : content opacity

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 
 // Side bar :
 const sideBar = document.querySelector(".side-bar");
-// Filtre opaque devant le contenu :
-const darkFilter = document.querySelector(".dark-filter");
+// Contenu :
+const content = document.querySelector(".content");
 
 // Ouverture/fermeture de la sidebar au click sur hamburger :
 btn.addEventListener("click", () => {
   sideBar.classList.toggle("visible");
-  darkFilter.classList.toggle("dark-filter-on");
   // apparence du "burger" !
   btn.classList.toggle("open");
 });
@@ -16,9 +15,8 @@ btn.addEventListener("click", () => {
 // Ranger la sidebar si on click sur le contenu principal
 
 // Disparition de la sidebar au click à coté :
-darkFilter.addEventListener("click", () => {
+content.addEventListener("click", () => {
   sideBar.classList.toggle("visible");
-  darkFilter.classList.toggle("dark-filter-on");
   // apparence du "burger" !
   btn.classList.remove("open");
 });
